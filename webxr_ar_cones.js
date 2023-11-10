7cd5f68f06ba91dbf8d818c478d42a69d68ec93a
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-10 11:42:30 +0100

    update

--- webxr_ar_cones.js
+++ webxr_ar_cones.js
@@ -1,4 +1,6 @@
-import * as THREE from 'three';
+// import * as THREE from 'three';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+
 class ARButton {
 
 	static createButton( renderer, sessionInit = {} ) {
