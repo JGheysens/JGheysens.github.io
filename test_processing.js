e8839e9f0c957ac6a5ca857d2b10762586f3c949
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-16 15:50:47 +0100

    Update test_processing.js

--- test_processing.js
+++ test_processing.js
@@ -3,7 +3,7 @@ import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/thr
 
 const fftSize=128;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.Camera();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -54,17 +54,6 @@ scene.add( cube ); */
 
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
-
-    
-
-	/* cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01; */
-
-	render();
-}
-
 function render() {
 
     analyser.getFrequencyData();
@@ -74,4 +63,13 @@ function render() {
     renderer.render( scene, camera );
 
 }
+
+function animate() {
+	requestAnimationFrame( animate );
+	/* cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01; */
+	render();
+}
+
+
 animate();
