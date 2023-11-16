eacf10b745a6696d7880c29ac34ca30986c8ce4a
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-16 15:48:44 +0100

    Update test_processing.js

--- test_processing.js
+++ test_processing.js
@@ -1,7 +1,7 @@
 // import * as THREE from 'three';
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
-fftSize=128;
+const fftSize=128;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -9,8 +9,13 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const listener = new THREE.AudioListener();
 
+const overlay = document.getElementById( 'overlay' );
+overlay.remove();
+
+const container = document.getElementById( 'container' );
+
+const listener = new THREE.AudioListener();
 const audio = new THREE.Audio( listener );
 const file = './sounds/drums.mp3';
 const mediaElement = new Audio( file );
@@ -29,18 +34,18 @@ uniforms = {
 
 };
 
-    const material = new THREE.ShaderMaterial( {
+const material = new THREE.ShaderMaterial( {
 
-        uniforms: uniforms,
-        vertexShader: document.getElementById( 'vertexShader' ).textContent,
-        fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+    uniforms: uniforms,
+    vertexShader: document.getElementById( 'vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 
-    } );
+} );
 
-    const geometry = new THREE.PlaneGeometry( 1, 1 );
+const geometry = new THREE.PlaneGeometry( 1, 1 );
 
-    const mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
 /* 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
