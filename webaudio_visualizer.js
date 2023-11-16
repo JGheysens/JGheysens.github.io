a70f002bb94b2ed90afb38e0da25091ce199050c
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-16 16:34:10 +0100

    Update webaudio_visualizer.js

--- webaudio_visualizer.js
+++ webaudio_visualizer.js
@@ -1,5 +1,32 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
+
+// Shader code as strings
+const vertexShaderCode = `
+    varying vec2 vUv;
+
+    void main() {
+        vUv = uv;
+        gl_Position = vec4(position, 1.0);
+    }
+`;
+
+const fragmentShaderCode = `
+    uniform sampler2D tAudioData;
+    varying vec2 vUv;
+
+    void main() {
+        vec3 backgroundColor = vec3(0.125, 0.125, 0.125);
+        vec3 color = vec3(1.0, 1.0, 0.0);
+
+        float f = texture2D(tAudioData, vec2(vUv.x, 0.0)).r;
+
+        float i = step(vUv.y, f) * step(f - 0.0125, vUv.y);
+
+        gl_FragColor = vec4(mix(backgroundColor, color, i), 1.0);
+    }
+`;
+
 const fftSize = 128;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -8,6 +35,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Assuming you have an HTML element with id 'overlay'
+const overlay = document.getElementById('overlay');
+overlay.remove();
+
+// Assuming you have an HTML element with id 'container'
+const container = document.getElementById('container');
 
 const listener = new THREE.AudioListener();
 const audio = new THREE.Audio(listener);
@@ -24,12 +57,11 @@ let uniforms = {
 };
 
 // Shader material with inline shader code
-/* const material = new THREE.ShaderMaterial({
+const material = new THREE.ShaderMaterial({
     uniforms: uniforms,
     vertexShader: vertexShaderCode,
     fragmentShader: fragmentShaderCode
-}); */
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+}); 
 const geometry = new THREE.PlaneGeometry(5, 5); // Increase the size of the plane
 
 const mesh = new THREE.Mesh(geometry, material);
