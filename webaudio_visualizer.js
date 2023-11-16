313022928bc26e9571a8e7f1c8f7590f2d41cbc3
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-16 16:38:42 +0100

    Update webaudio_visualizer.js

--- webaudio_visualizer.js
+++ webaudio_visualizer.js
@@ -27,51 +27,112 @@ const fragmentShaderCode = `
     }
 `;
 
-const fftSize = 128;
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let scene, camera, renderer, analyser, uniforms;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const startButton = document.getElementById( 'startButton' );
+startButton.addEventListener( 'click', init );
 
+function init() {
 
-const listener = new THREE.AudioListener();
-const audio = new THREE.Audio(listener);
-const file = './sounds/drums.mp3';
-const mediaElement = new Audio(file);
-mediaElement.play();
-audio.setMediaElementSource(mediaElement);
+    const fftSize = 128;
 
-const analyser = new THREE.AudioAnalyser(audio, fftSize);
+    //
 
-// Use 'let' to declare variables
-let uniforms = {
-    tAudioData: { value: new THREE.DataTexture(analyser.data, fftSize / 2, 1, THREE.RedFormat) }
-};
+    const overlay = document.getElementById( 'overlay' );
+    overlay.remove();
 
-// Shader material with inline shader code
-const material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
-    vertexShader: vertexShaderCode,
-    fragmentShader: fragmentShaderCode
-}); 
-const geometry = new THREE.PlaneGeometry(5, 5); // Increase the size of the plane
+    //
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+    const container = document.getElementById( 'container' );
 
-camera.position.z = 5;
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
 
-function render() {
-    analyser.getFrequencyData();
-    uniforms.tAudioData.value.needsUpdate = true;
-    renderer.render(scene, camera);
+    scene = new THREE.Scene();
+
+    camera = new THREE.Camera();
+
+    //
+
+    const listener = new THREE.AudioListener();
+
+    const audio = new THREE.Audio( listener );
+    const file = './sounds/376737_Skullbeatz___Bad_Cat_Maste.mp3';
+
+    if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
+
+        const loader = new THREE.AudioLoader();
+        loader.load( file, function ( buffer ) {
+
+            audio.setBuffer( buffer );
+            audio.play();
+
+        } );
+
+    } else {
+
+        const mediaElement = new Audio( file );
+        mediaElement.play();
+
+        audio.setMediaElementSource( mediaElement );
+
+    }
+
+    analyser = new THREE.AudioAnalyser( audio, fftSize );
+
+    //
+
+    const format = ( renderer.capabilities.isWebGL2 ) ? THREE.RedFormat : THREE.LuminanceFormat;
+
+    uniforms = {
+
+        tAudioData: { value: new THREE.DataTexture( analyser.data, fftSize / 2, 1, format ) }
+
+    };
+
+    const material = new THREE.ShaderMaterial( {
+
+        uniforms: uniforms,
+        vertexShader: document.getElementById( 'vertexShader' ).textContent,
+        fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+
+    } );
+
+    const geometry = new THREE.PlaneGeometry( 1, 1 );
+
+    const mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize );
+
+    animate();
+
+}
+
+function onWindowResize() {
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+
+    requestAnimationFrame( animate );
+
     render();
+
 }
 
-animate();
+function render() {
+
+    analyser.getFrequencyData();
+
+    uniforms.tAudioData.value.needsUpdate = true;
+
+    renderer.render( scene, camera );
+
+}
