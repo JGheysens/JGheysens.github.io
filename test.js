bd75faa2b23e971f3c2aced9cd2d78bcafafffea
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-16 17:09:43 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,45 +1,62 @@
 // import * as THREE from 'three';
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+let scene, camera, renderer, analyser, uniforms;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const startButton = document.getElementById( 'startButton' );
+startButton.addEventListener( 'click', init );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+function init(){
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
 
-const listener = new THREE.AudioListener();
-const audio = new THREE.Audio(listener);
-const file = './sounds/drums.mp3';
-if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
+	const listener = new THREE.AudioListener();
+	const audio = new THREE.Audio(listener);
+	const file = './sounds/drums.mp3';
+	if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
 
-	const loader = new THREE.AudioLoader();
-	loader.load( file, function ( buffer ) {
+		const loader = new THREE.AudioLoader();
+		loader.load( file, function ( buffer ) {
 
-		audio.setBuffer( buffer );
-		audio.play();
+			audio.setBuffer( buffer );
+			audio.play();
 
-	} );
+		} );
 
-} else {
+	} else {
 
-	const mediaElement = new Audio( file );
-	mediaElement.play();
+		const mediaElement = new Audio( file );
+		mediaElement.play();
 
-	audio.setMediaElementSource( mediaElement );
+		audio.setMediaElementSource( mediaElement );
+
+	}
+	analyser = new THREE.AudioAnalyser( audio, fftSize );
+
+	const geometry = new THREE.PlaneGeometry( 10, 10 );
+	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	const mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+	/* const cube = new THREE.Mesh( geometry, material );
+	scene.add( cube ); */
+
+	camera.position.z = 5;
+
+	window.addEventListener( 'resize', onWindowResize );
+
+    animate();
+}
+
+function onWindowResize() {
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
-analyser = new THREE.AudioAnalyser( audio, fftSize );
 
-const geometry = new THREE.PlaneGeometry( 10, 10 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-/* const cube = new THREE.Mesh( geometry, material );
-scene.add( cube ); */
-
-camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
@@ -65,5 +82,3 @@ playButton.addEventListener('click', () => {
         audio.setMediaElementSource(mediaElement);
     }
 }); */
-
-animate();
