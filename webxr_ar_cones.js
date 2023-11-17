6b757a6a7399b4d4110fc74ccb51654fa793ee1d
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-17 10:58:10 +0100

    Update webxr_ar_cones.js

--- webxr_ar_cones.js
+++ webxr_ar_cones.js
@@ -209,79 +209,79 @@ class ARButton {
 }
 
 let camera, scene, renderer;
-let controller;
+			let controller;
 
-init();
-animate();
+			init();
+			animate();
 
-function init() {
+			function init() {
 
-    const container = document.createElement( 'div' );
-    document.body.appendChild( container );
+				const container = document.createElement( 'div' );
+				document.body.appendChild( container );
 
-    scene = new THREE.Scene();
+				scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 3 );
-    light.position.set( 0.5, 1, 0.25 );
-    scene.add( light );
+				const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 3 );
+				light.position.set( 0.5, 1, 0.25 );
+				scene.add( light );
 
-    //
+				//
 
-    renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.xr.enabled = true;
-    container.appendChild( renderer.domElement );
+				renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.xr.enabled = true;
+				container.appendChild( renderer.domElement );
 
-    //
+				//
 
-    document.body.appendChild( ARButton.createButton( renderer ) );
+				document.body.appendChild( ARButton.createButton( renderer ) );
 
-    //
+				//
 
-    const geometry = new THREE.CylinderGeometry( 0, 0.05, 0.2, 32 ).rotateX( Math.PI / 2 );
+				const geometry = new THREE.CylinderGeometry( 0, 0.05, 0.2, 32 ).rotateX( Math.PI / 2 );
 
-    function onSelect() {
+				function onSelect() {
 
-        const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-        const mesh = new THREE.Mesh( geometry, material );
-        mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
-        mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
-        scene.add( mesh );
+					const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+					mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+					scene.add( mesh );
 
-    }
+				}
 
-    controller = renderer.xr.getController( 0 );
-    controller.addEventListener( 'select', onSelect );
-    scene.add( controller );
+				controller = renderer.xr.getController( 0 );
+				controller.addEventListener( 'select', onSelect );
+				scene.add( controller );
 
-    //
+				//
 
-    window.addEventListener( 'resize', onWindowResize );
+				window.addEventListener( 'resize', onWindowResize );
 
-}
+			}
 
-function onWindowResize() {
+			function onWindowResize() {
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.setSize( window.innerWidth, window.innerHeight );
 
-}
+			}
 
-//
+			//
 
-function animate() {
+			function animate() {
 
-    renderer.setAnimationLoop( render );
+				renderer.setAnimationLoop( render );
 
-}
+			}
 
-function render() {
+			function render() {
 
-    renderer.render( scene, camera );
+				renderer.render( scene, camera );
 
-}
+			}
