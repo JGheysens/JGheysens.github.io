65b434d5c2c5196123c02efd7ae6c393d721bf03
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-28 11:12:47 +0100

    Update test.js

--- test.js
+++ test.js
@@ -291,7 +291,8 @@ function init() {
   // Create a new AudioAnalyser object
   const analyser = new THREE.AudioAnalyser(audio1, 32);
   analyser.analyser.fftSize = 128;
-  const data = analyser.getFrequencyData();
+  const data = analyser.data;
+  const value = new THREE.DataTexture(data, fftSize/2, 11, THREE.LuminanceFormat);
 
 
 
@@ -302,13 +303,13 @@ function init() {
 	new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide }), // Blue material for plane3
 	new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide }), // Yellow material for plane4
   ];
+  planeMaterials[0].map = new THREE.DataTexture(value);
 
   // Create the first plane and position it
   const geometry1 = new THREE.PlaneGeometry(0.5, 0.5);
   plane1 = new THREE.Mesh(geometry1, planeMaterials[0]);
   plane1.position.set(-1, 0, -1); // Move the first plane to the left
   plane1.rotateY(Math.PI / 4); // Rotate the plane 45 degrees
-  planeMaterials[0].map = new THREE.DataTexture(data, 16, 16, THREE.LuminanceFormat);
   scene.add(plane1);
 
   // Create the second plane and position it
