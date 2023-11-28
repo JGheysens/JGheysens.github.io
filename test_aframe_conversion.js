589498b58d2167a7f846f1a23a970083be7ac736
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-28 15:26:18 +0100

    update

--- test_aframe_conversion.js
+++ test_aframe_conversion.js
@@ -7,8 +7,9 @@ const startButton = document.getElementById('startButton');
 startButton.addEventListener('click', init);
 
 function init() {
-  const container = document.createElement('div');
-  document.body.appendChild(container);
+    const container = document.getElementById('container');
+            const playAudioButton = document.getElementById('playAudioButton');
+            playAudioButton.addEventListener('click', playAudio);
 
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -51,7 +52,31 @@ function init() {
   animate();
 }
 
+function playAudio() {
+    const listener = new THREE.AudioListener();
 
+const audio = new THREE.Audio( listener );
+const file = './sounds/drums.mp3';
+
+if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
+
+    const loader = new THREE.AudioLoader();
+    loader.load( file, function ( buffer ) {
+
+        audio.setBuffer( buffer );
+        audio.play();
+
+    } );
+
+} else {
+
+    const mediaElement = new Audio( file );
+    mediaElement.play();
+
+    audio.setMediaElementSource( mediaElement );
+
+};
+}
 
 function animate() {
     requestAnimationFrame(animate);
