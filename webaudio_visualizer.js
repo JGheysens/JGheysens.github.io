18157664987ac15e852f36537476cb3b2fa04fa3
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-28 14:18:44 +0100

    Update webaudio_visualizer.js

--- webaudio_visualizer.js
+++ webaudio_visualizer.js
@@ -1,211 +1,5 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
-class ARButton {
-
-	static createButton( renderer, sessionInit = {} ) {
-
-		const button = document.createElement( 'button' );
-
-		function showStartAR( /*device*/ ) {
-
-			if ( sessionInit.domOverlay === undefined ) {
-
-				const overlay = document.createElement( 'div' );
-				overlay.style.display = 'none';
-				document.body.appendChild( overlay );
-
-				const svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
-				svg.setAttribute( 'width', 38 );
-				svg.setAttribute( 'height', 38 );
-				svg.style.position = 'absolute';
-				svg.style.right = '20px';
-				svg.style.top = '20px';
-				svg.addEventListener( 'click', function () {
-
-					currentSession.end();
-
-				} );
-				overlay.appendChild( svg );
-
-				const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
-				path.setAttribute( 'd', 'M 12,12 L 28,28 M 28,12 12,28' );
-				path.setAttribute( 'stroke', '#fff' );
-				path.setAttribute( 'stroke-width', 2 );
-				svg.appendChild( path );
-
-				if ( sessionInit.optionalFeatures === undefined ) {
-
-					sessionInit.optionalFeatures = [];
-
-				}
-
-				sessionInit.optionalFeatures.push( 'dom-overlay' );
-				sessionInit.domOverlay = { root: overlay };
-
-			}
-
-			//
-
-			let currentSession = null;
-
-			async function onSessionStarted( session ) {
-
-				session.addEventListener( 'end', onSessionEnded );
-
-				renderer.xr.setReferenceSpaceType( 'local' );
-
-				await renderer.xr.setSession( session );
-
-				button.textContent = 'STOP AR';
-				sessionInit.domOverlay.root.style.display = '';
-
-				currentSession = session;
-
-			}
-
-			function onSessionEnded( /*event*/ ) {
-
-				currentSession.removeEventListener( 'end', onSessionEnded );
-
-				button.textContent = 'START AR';
-				sessionInit.domOverlay.root.style.display = 'none';
-
-				currentSession = null;
-
-			}
-
-			//
-
-			button.style.display = '';
-
-			button.style.cursor = 'pointer';
-			button.style.left = 'calc(50% - 50px)';
-			button.style.width = '100px';
-
-			button.textContent = 'START AR';
-
-			button.onmouseenter = function () {
-
-				button.style.opacity = '1.0';
-
-			};
-
-			button.onmouseleave = function () {
-
-				button.style.opacity = '0.5';
-
-			};
-
-			button.onclick = function () {
-
-				if ( currentSession === null ) {
-
-					navigator.xr.requestSession( 'immersive-ar', sessionInit ).then( onSessionStarted );
-
-				} else {
-
-					currentSession.end();
-
-				}
-
-			};
-
-		}
-
-		function disableButton() {
-
-			button.style.display = '';
-
-			button.style.cursor = 'auto';
-			button.style.left = 'calc(50% - 75px)';
-			button.style.width = '150px';
-
-			button.onmouseenter = null;
-			button.onmouseleave = null;
-
-			button.onclick = null;
-
-		}
-
-		function showARNotSupported() {
-
-			disableButton();
-
-			button.textContent = 'AR NOT SUPPORTED';
-
-		}
-
-		function showARNotAllowed( exception ) {
-
-			disableButton();
-
-			console.warn( 'Exception when trying to call xr.isSessionSupported', exception );
-
-			button.textContent = 'AR NOT ALLOWED';
-
-		}
-
-		function stylizeElement( element ) {
-
-			element.style.position = 'absolute';
-			element.style.bottom = '20px';
-			element.style.padding = '12px 6px';
-			element.style.border = '1px solid #fff';
-			element.style.borderRadius = '4px';
-			element.style.background = 'rgba(0,0,0,0.1)';
-			element.style.color = '#fff';
-			element.style.font = 'normal 13px sans-serif';
-			element.style.textAlign = 'center';
-			element.style.opacity = '0.5';
-			element.style.outline = 'none';
-			element.style.zIndex = '999';
-
-		}
-
-		if ( 'xr' in navigator ) {
-
-			button.id = 'ARButton';
-			button.style.display = 'none';
-
-			stylizeElement( button );
-
-			navigator.xr.isSessionSupported( 'immersive-ar' ).then( function ( supported ) {
-
-				supported ? showStartAR() : showARNotSupported();
-
-			} ).catch( showARNotAllowed );
-
-			return button;
-
-		} else {
-
-			const message = document.createElement( 'a' );
-
-			if ( window.isSecureContext === false ) {
-
-				message.href = document.location.href.replace( /^http:/, 'https:' );
-				message.innerHTML = 'WEBXR NEEDS HTTPS'; // TODO Improve message
-
-			} else {
-
-				message.href = 'https://immersiveweb.dev/';
-				message.innerHTML = 'WEBXR NOT AVAILABLE';
-
-			}
-
-			message.style.left = 'calc(50% - 90px)';
-			message.style.width = '180px';
-			message.style.textDecoration = 'none';
-
-			stylizeElement( message );
-
-			return message;
-
-		}
-
-	}
-
-}
 
 // Shader code as strings
 const vertexShaderCode = `
@@ -245,18 +39,12 @@ function init() {
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.xr.enabled = true;
     container.appendChild( renderer.domElement );
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+    camera = new THREE.Camera();
 
-    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 3);
-    light.position.set(0.5, 1, 0.25);
-    scene.add(light);
-
-    document.body.appendChild(ARButton.createButton(renderer));
 
     const listener = new THREE.AudioListener();
 
@@ -284,8 +72,6 @@ function init() {
 
     }
 
-    camera.add( listener );
-
     analyser = new THREE.AudioAnalyser( audio, fftSize );
 
     //
@@ -309,20 +95,9 @@ function init() {
     const geometry = new THREE.PlaneGeometry( 1, 1 );
 
     const mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set(-1, 0, -1); // Move the first plane to the left
-    mesh.rotateY(Math.PI / 4); // Rotate the plane 45 degrees
     scene.add( mesh );
 
-    materialPlane = new THREE.MeshBasicMaterial( {color: 0x00ff00, side: THREE.DoubleSide} );
-    plane1 = new THREE.Mesh( geometry, materialPlane );
-    plane1.position.set(0, 0, 1); // Move the first plane to the left
-    plane1.rotateY(Math.PI / 4); // Rotate the plane 45 degrees
-    scene.add( plane1 );
-
     //
-    controller = renderer.xr.getController(0);
-    controller.addEventListener('select', onSelect);
-    scene.add(controller);
 
     window.addEventListener( 'resize', onWindowResize );
 
@@ -330,54 +105,7 @@ function init() {
 
 }
 
-function onSelect() {
-	const intersections = getIntersections(controller);
-  
-	if (intersections.length > 0) {
-	  const intersectedObject = intersections[0].object;
-  
-	  // Pause and play the audio to trigger a restart
-	  if (intersectedObject == plane1) {
-		toggleAudio(audio1);
-	  }
-    }
-  }
-
-function toggleAudio(audio) {
-if (isplaying) {
-    audio.pause();
-    isplaying = false;
-} else {
-    audio.play();
-    isplaying = true;
-}
-}
-
-function getIntersections(controller) {
-	const tempMatrix = new THREE.Matrix4();
-	const raycaster = new THREE.Raycaster();
-	const intersections = [];
-  
-	// Update the raycaster with the controller's position and direction
-	const controllerMatrix = controller.matrixWorld;
-	raycaster.ray.origin.setFromMatrixPosition(controllerMatrix);
-	raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix.identity().extractRotation(controllerMatrix));
-  
-	// Check for intersections with each plane
-	const planes = [plane1, plane2, plane3, plane4];
-	for (const plane of planes) {
-	  const intersection = raycaster.intersectObject(plane);
-	  if (intersection.length > 0) {
-		intersections.push(intersection[0]);
-	  }
-	}
-  
-	return intersections;
-  }
-
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 
