f98183d45ba5d2c533099bc8c80e0e6827c74324
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-30 13:20:34 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -16,6 +16,12 @@ let screenSize;
 // Set up the initial scene
 function init() {
 
+    const overlay = document.getElementById('overlay');
+    overlay.remove();
+
+            //
+    const container = document.getElementById('container');
+
     // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
