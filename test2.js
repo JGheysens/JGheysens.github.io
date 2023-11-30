93cd8b5d940eaa0bb2da7ebec2953b31bb311cfb
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-30 14:34:45 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -29,7 +29,7 @@ function init() {
     // Set up the scene, camera, and renderer
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
