482b1bd4eb0b5cae4fc5902e0dc19bb28b927693
JGheysens <116629526+JGheysens@users.noreply.github.com>
2023-11-30 13:25:23 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -19,9 +19,35 @@ function init() {
     const overlay = document.getElementById('overlay');
     overlay.remove();
 
-            //
+    
     const container = document.getElementById('container');
+    const playAudioButton = document.getElementById('playAudioButton');
+    playAudioButton.addEventListener('click', playAudio);
+    function playAudio() {
+        const listener = new THREE.AudioListener();
 
+    const audio = new THREE.Audio( listener );
+    const file = './sounds/drums.mp3';
+
+    if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
+
+        const loader = new THREE.AudioLoader();
+        loader.load( file, function ( buffer ) {
+
+            audio.setBuffer( buffer );
+            audio.play();
+
+        } );
+
+    } else {
+
+        const mediaElement = new Audio( file );
+        mediaElement.play();
+        mediaElement.loop = true;
+        audio.setMediaElementSource( mediaElement );
+
+    };
+    }
     // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
